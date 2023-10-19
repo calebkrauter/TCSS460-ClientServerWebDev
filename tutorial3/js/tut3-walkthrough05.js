783c4ef6e000c4a38faf200bb539c29c00e34315
Caleb Krauter <krautercaleb@gmail.com>
2023-10-19 10:05:24 -0700

    Tutorial 3

--- tutorial3/js/tut3-walkthrough05.js
+++ tutorial3/js/tut3-walkthrough05.js
@@ -1,2 +1,51 @@
 /* put your DOM code here */
+// $(function () {
+//     // create a new DOM element
+//     var img = $('<img src="images/art/thumbs/13030.jpg" >');
+//     // and now add the new element after the panel
+//     var panel = $('.panel');
+//     panel.after(img);
+//     });
 
+//     $(function () {
+//         // create a new DOM element
+//         var img = $('<img src="images/art/thumbs/13030.jpg" >');
+//         // and now add the new element after the panel
+//         var panel = $('.panel');
+//         panel.before(img);
+//     });
+//     $(function () {
+//         // create a new DOM element
+//         var img = $('<img src="images/art/thumbs/13030.jpg" >');
+//         // and now add the new element after the panel
+//         var panel = $('.panel');
+//         panel.append(img);
+//     });
+//     $(function () {
+//         // create a new DOM element
+//         var img = $('<img src="images/art/thumbs/13030.jpg" >');
+//         // and now add the new element after the panel
+//         var panel = $('.panel');
+//         panel.prepend(img);
+//     });
+// $(function () {
+//     // create a new DOM element
+//     var img = $('<img src="images/art/thumbs/13030.jpg" >');
+//     // and now add the new element after the panel
+//     var panel = $('.panel');
+//     img.appendTo(panel);
+// });
+// $(function () {
+//     // create a new DOM element
+//     var img = $('<img src="images/art/thumbs/13030.jpg" >');
+//     // and now add the new element after the panel
+//     var panel = $('.panel');
+//     img.prependTo(panel);
+// });
+$(function () {
+    // create a new DOM element
+    var img = $('<img src="images/art/thumbs/13030.jpg" >');
+    // and now add the new element after the panel
+    var panel = $('.panel');
+    img.insertBefore(panel);
+});
